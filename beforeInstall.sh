3295d985834af88d70e7de53458dab26b163c5d1
mmojtaba <44787178+MMojtaba@users.noreply.github.com>
2023-06-15 12:02:45 -0700

    Deploying backend

--- beforeInstall.sh
+++ beforeInstall.sh
@@ -2,3 +2,5 @@
 echo "Running before install commands."
 sudo apt-get update
 sudo apt-get -y install make
+echo "Unzipping built files."
+unzip -o appFiles.zip -d .
--- dummyScript.sh
+++ dummyScript.sh
@@ -2,4 +2,3 @@
 echo "Dummy script"
 pwd
 ls
-ls /opt/codedeploy-agent/deployment-root
