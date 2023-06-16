4bba1abfbae782ac749d700f3554434ee1aa99b9
mmojtaba <44787178+MMojtaba@users.noreply.github.com>
2023-06-16 09:48:41 -0700

    Fixing frontend deployment problems

--- frontend_stop.sh
+++ frontend_stop.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-killall node > /dev/null
+if pgrep node &> /dev/null ; then
+        killall node;
+fi
 # kill -9 $(ps aux | grep '\snode\s' | awk '{print $2}') || echo "Frontend already stopped"
 
 # (! $(ps aux | grep '\snode\s' | awk '{print $2}')) || kill -9 $(ps aux | grep '\snode\s' | awk '{print $2}')
