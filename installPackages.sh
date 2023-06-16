7723ba31ed90a312458f209b2c64d1ecf1227063
mmojtaba <44787178+MMojtaba@users.noreply.github.com>
2023-06-15 19:46:40 -0700

    Adding frontend to codedeploy

--- installPackages.sh
+++ installPackages.sh
@@ -12,7 +12,7 @@ sudo apt-get -y install docker-compose
 # fi
 
 echo "before before"
-sudo bash ~/.nvm/nvm.sh
+cd ~/.nvm && . ./nvm.sh
 # export NVM_DIR="$HOME/.nvm"
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
