2f12ac954e74f7ad8c0cff09ebd1c0e9d79bda4d
mmojtaba <44787178+MMojtaba@users.noreply.github.com>
2023-06-16 08:19:23 -0700

    fixing frontend stop giving error

--- frontend_stop.sh
+++ frontend_stop.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+# killall -9 node
+# kill -9 $(ps aux | grep '\snode\s' | awk '{print $2}') || echo "Frontend already stopped"
+
+# (! $(ps aux | grep '\snode\s' | awk '{print $2}')) || kill -9 $(ps aux | grep '\snode\s' | awk '{print $2}')
