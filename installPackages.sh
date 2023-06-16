262e4c9a3a9c1269d0373079cd4753c11764179c
mmojtaba <44787178+MMojtaba@users.noreply.github.com>
2023-06-15 19:13:46 -0700

    Adding frontend to codedeploy

--- installPackages.sh
+++ installPackages.sh
@@ -17,5 +17,5 @@ sudo bash ~/.nvm/nvm.sh
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 echo "before"
-nvm install latest && true
-nvm use latest && true
+cd ~/.nvm && nvm install latest && true
+cd ~/.nvm && nvm use latest && true
