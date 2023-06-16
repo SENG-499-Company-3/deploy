e4c85f3d5f4bcad68f9b6624b1c56f39c55ab244
mmojtaba <44787178+MMojtaba@users.noreply.github.com>
2023-06-16 10:31:25 -0700

    Fixing frontend being in running state causing timeout

--- frontend_start.sh
+++ frontend_start.sh
@@ -5,4 +5,5 @@ cd ./frontend
 npm install 
 npm run build
 #npm run dev > tmpFE.txt &
-npm run --silent start > tmpFE.txt &
+#npm run --silent start > tmpFE.txt &
+npm run start > /dev/null 2> /dev/null < /dev/null &
