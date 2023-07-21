7a6c0b6cebe16465bc98c25975f6d7947d06a82b
mmojtaba <44787178+MMojtaba@users.noreply.github.com>
2023-07-21 12:26:58 -0700

    Update algo1 build to use --build

--- alg1_start.sh
+++ alg1_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ./algorithm-1
 #docker compose up
-nohup sudo docker-compose up > /dev/null 2> /dev/null < /dev/null &
+nohup sudo docker-compose up --build > /dev/null 2> /dev/null < /dev/null &
